89f8e1bf58018a9e095e7b5aeb9b83f3e82390d8
Srierin <3315853605@qq.com>
2025-06-07 14:06:44 +0800

    react组件

---
+++ react/todoListComponents/src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import '../Todo.css'
+import TodoForm from './TodoForm'
+import Todos from './Todos'
+function TodoList() {
+    return (
+       <div className="container">
+         <h1 className="title">Todo List</h1>
+         <TodoForm />
+         <Todos />
+       </div>
+    )
+}
+export default TodoList;
