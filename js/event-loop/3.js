a56c77b9c60da255eac09d7711449fa5fe5cdc20
Srierin <3315853605@qq.com>
2025-07-12 17:37:40 +0800

    前后端联系 多进程多线程

--- js/event-loop/3.js
+++ js/event-loop/3.js
@@ -2,12 +2,13 @@ console.log('Start');
 // node 微任务
 // process 是进程对象，nextTick 是一个方法，会将回调函数放入到微任务队列中
 
-process.nextTick(() => {
-  console.log('Process next tick');
-})
 // 微任务
 Promise.resolve().then(() => {
   console.log('Promise resolved');
+})
+
+process.nextTick(() => {
+  console.log('Process next tick');
 })
 
 // 宏任务
