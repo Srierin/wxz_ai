69b6042c4acd705f24c764efc2f7b551ca9aa45b
Srierin <3315853605@qq.com>
2025-08-23 00:57:47 +0800

    浏览器缓存，Prisma初识

---
+++ interview/http/cache/cache-demo/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (request, response) {
+  if (request.url === '/') {
+    // index.html
+    // async 异步 sync 同步
+    // 同步性能差点 
+    const html = fs.readFileSync('test.html', 'utf-8');
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    response.end(html);
+  }
+  if (request.url === '/script.js') {
+    response.writeHead(200, {
+      'Content-Type': 'application/javascript',
+      'Expires': new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toGMTString()
+      // 'Cache-Control': 'max-age=20,public'
+    })
+    const script = fs.readFileSync('script.js', 'utf-8');
+    response.end(script);
+  }
+})
+  .listen(8888)
